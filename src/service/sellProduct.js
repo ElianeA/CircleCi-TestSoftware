a06ec00763334030097e8af54200679bf38e3363
Eliane <elianevia@gmail.com>
2021-10-06 14:22:11 -0300

    Intial Commit

---
+++ src/service/sellProduct.js
@@ -0,0 +1,12 @@
+/**
+ * A quantiade vendida pode ser 1 ou mais unidades,
+ * --> Se estoque ficar negativo uma exception deve ser lançada
+ * --> O valor de venda não pode ser maior que o valor de compra
+ * @param {*} product
+ * @param {*} amount
+ * */
+
+export default function sellProduct(product, amount) {
+    product.stock -= 1;
+    return product;
+}
